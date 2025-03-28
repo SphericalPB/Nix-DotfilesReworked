31d1196100bfc1af7fac4ad742361ea929bb69af
SphericalPB <xdmaxgamer360@gmail.com>
2025-03-28 18:32:29 +0800

    2025-03-28_103229

--- home-manager/programs/wayland/wofi/settings.nix
+++ home-manager/programs/wayland/wofi/settings.nix
@@ -2,6 +2,7 @@
   programs.wofi.settings = {
     show = "drun";
     prompt = "What'cha wanna do today?";
+    insensitive = true;
     allow_images = true;
     key_left = "H";
     key_down = "J";
