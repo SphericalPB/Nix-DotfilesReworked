d3da7f606d98191720fc48ef6fde8efe1b7aa34c
SphericalPB <xdmaxgamer360@gmail.com>
2025-04-01 15:27:42 +0800

    2025-04-01_072742

--- home-manager/programs/flatpak.nix
+++ home-manager/programs/flatpak.nix
@@ -26,6 +26,7 @@
         flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
         sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
       }
+      "com.usebottles.bottles"
     ];
   };
 }
