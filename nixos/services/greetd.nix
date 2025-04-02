35b4a4b690e1a1e9b334ebe7356782c505ba2123
SphericalPB <xdmaxgamer360@gmail.com>
2025-04-02 15:24:15 +0800

    2025-04-02_072415

--- nixos/services/greetd.nix
+++ nixos/services/greetd.nix
@@ -5,23 +5,17 @@
 }: {
   services.greetd = {
     enable = true;
-    settings = {
-      #default_session = {
-      #command = "${lib.getExe config.programs.hyprland.package} --config /etc/greetd/hyprland.conf";
-      #user = "greeter";
-      #};
-    };
   };
 
   # Hyprland configuration, to be used by greetd using hyprland as its compositor
-  environment.etc."greetd/hyprland.conf".text = ''
-    exec-once = regreet; hyprctl dispatch exit
-    misc {
-      disable_hyprland_logo = true
-      disable_splash_rendering = true
-      disable_hyprland_qtutils_check = true
-    }
-  '';
+  #environment.etc."greetd/hyprland.conf".text = ''
+  #  exec-once = regreet; hyprctl dispatch exit
+  #  misc {
+  #    disable_hyprland_logo = true
+  #    disable_splash_rendering = true
+  #    disable_hyprland_qtutils_check = true
+  #  }
+  #'';
 
   programs.regreet = {
     enable = true;
