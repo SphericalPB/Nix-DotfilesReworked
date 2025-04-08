12c3d7c53e90b3d13b2321cc28e1d4a572c59646
SphericalPB <xdmaxgamer360@gmail.com>
2025-04-08 16:47:54 +0800

    2025-04-08_084754

--- home-manager/terminal/shell/scripts/rebuild-switch.sh
+++ home-manager/terminal/shell/scripts/rebuild-switch.sh
@@ -72,7 +72,7 @@ rebuild $updateFlag
 # If skipCommit is an empty string, then do commit and push procedure
 # Else, skip it entirely.
 if [[ -z "$skipCommit" ]]; then
-  git diff HEAD~1 . | delta # show the changes between the current and the previous config version
+  git diff | delta # show the changes between the current and the previous config version
   echo =============================
   echo 'Please enter a commit name? (keep empty for timestamp)' 
   read -p '> '  commitName
