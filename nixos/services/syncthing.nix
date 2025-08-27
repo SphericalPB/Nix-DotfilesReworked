833bae01917c5868de00fb63533f542c1965b42e
SphericalPB <xdmaxgamer360@gmail.com>
2025-08-27 19:19:21 +0800

    more syncthing stuff

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -13,7 +13,8 @@
       devices = {
         #"Spherical-Tab" = {id = "7ADOJWZ-IJHRG2V-3Y6W3PN-BJAQLKI-6G6UATT-7FQO2LC-URU6ZCK-6QEZKAP";};
         "Spherical-Tablet" = {id = "GINCNFG-YPAOAJF-OK6ZAMR-YEKZM4O-SAOVY6F-TK2ASBP-DIOPHEB-PXJ6ZAZ";};
-        "Spherical-Phone" = {id = "K6POAM7-Z556ZKR-VLTG57G-J7LZNUG-FQ4TFEA-447R6WS-RLVKU7F-KCMEVA3";};
+        #"Spherical-Phone" = {id = "K6POAM7-Z556ZKR-VLTG57G-J7LZNUG-FQ4TFEA-447R6WS-RLVKU7F-KCMEVA3";};
+        "Spherical-Phone" = {id = "YKH64SV-4TBW3SN-YEECLD3-7NAIJ4K-HQ6JVMD-CUWOCWL-LZEIPO5-JZTUFAW";};
       };
       folders = {
         "Pictures" = {
@@ -26,10 +27,14 @@
           };
         };
 
-        "Camera(DCIM)" = {
-          id = "stsnm-pjz1q";
+        "DCIM" = {
+          id = "n9xgp-9xssv";
           path = "/home/sphericalpb/DCIM/";
           devices = ["Spherical-Tablet" "Spherical-Phone"];
+          versioning = {
+            type = "trashcan";
+            params.cleanoutDays = "30";
+          };
         };
 
         "Music" = {
